0c6b65ec12d5b8ce042ca83ee8c7814a37995d6a
Alan Rizky Wardana <androidmaniac28@gmail.com>
2019-10-07 10:37:58 +0700

    Splash Screen

---
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/activities/SplashActivity.java
@@ -0,0 +1,31 @@
+package polinema.ac.id.androiduistarter.fragments.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Handler;
+import android.view.View;
+
+import polinema.ac.id.androiduistarter.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, WelcomeActivity.class));
+                finish();
+            }
+        }, 5000);
+    }
+}
